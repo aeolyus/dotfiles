c40fe976c787359921078ec8b981e6bfb5de38ab
Richard Huang <richardhuang.huang@gmail.com>
2023-04-17 01:36:38 -0700

    [nvim] Add indentation guides

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -89,6 +89,11 @@ _G.packer_plugins = {
     path = "/home/aeolyus/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
+  ["indent-blankline.nvim"] = {
+    loaded = true,
+    path = "/home/aeolyus/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    url = "https://github.com/lukas-reineke/indent-blankline.nvim"
+  },
   ["lsp-zero.nvim"] = {
     loaded = true,
     path = "/home/aeolyus/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
