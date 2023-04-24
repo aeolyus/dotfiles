fd5449643efb04ea4eb570554342ece7943ef1d1
Richard Huang <richardhuang.huang@gmail.com>
2023-04-23 20:52:05 -0700

    [nvim] Add telescope keymap for buffers and commands

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,5 +2,7 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-space>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+vim.keymap.set('n', '<C-c>', builtin.commands, {})
 
 require('telescope').load_extension('fzf')
