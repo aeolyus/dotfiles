f4c964b8842466ee40c3793b7b11947b73e65923
Richard Huang <richardhuang.huang@gmail.com>
2023-04-29 16:23:46 -0700

    [nvim] Allow treesitter highlighting to take higher priority than lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,6 +2,9 @@ local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({ buffer = bufnr })
+  -- Allow treesitter highlighting to take higher priority (100)
+  -- https://github.com/neovim/neovim/pull/22981
+  vim.highlight.priorities.semantic_tokens = 95
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
