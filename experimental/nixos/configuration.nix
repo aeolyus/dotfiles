93cd2ea7c54011ca68f5c15a73b56a8c8580cc4c
Richard Huang <richardhuang.huang@gmail.com>
2023-11-29 21:23:30 -0800

    [experimental] Enable networkmanager

--- experimental/nixos/configuration.nix
+++ experimental/nixos/configuration.nix
@@ -17,7 +17,7 @@
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
