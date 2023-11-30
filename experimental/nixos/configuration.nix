60554a2bd743684cbd9557805396d10ee368f813
Richard Huang <richardhuang.huang@gmail.com>
2023-11-29 21:24:01 -0800

    [experimental] Add vim and git as bare minimum tools

--- experimental/nixos/configuration.nix
+++ experimental/nixos/configuration.nix
@@ -68,10 +68,10 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
