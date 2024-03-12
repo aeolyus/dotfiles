93b9a70d3fddc3451479e1360ce17381558afa02
Richard Huang <richardhuang.huang@gmail.com>
2024-03-11 22:58:55 -0700

    [nix][nixos] Swap ctrl with caps

--- experimental/nixos/configuration.nix
+++ experimental/nixos/configuration.nix
@@ -38,7 +38,10 @@
   # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+    xkbOptions = "ctrl:swapcaps";
+  };
 
 
   # Enable the i3 window manager
