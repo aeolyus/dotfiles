8c8c90486b3d77d49782485ede315abca3ea470b
Richard Huang <richardhuang.huang@gmail.com>
2024-03-17 18:01:04 -0700

    [nix][nixos] Update xkbOptions to xkb.options

--- experimental/nixos/configuration.nix
+++ experimental/nixos/configuration.nix
@@ -40,7 +40,7 @@
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
-    xkbOptions = "ctrl:swapcaps";
+    xkb.options = "ctrl:swapcaps";
     # Use a fake session to let Home Manager take care of the X session
     # https://github.com/nix-community/home-manager/issues/391
     desktopManager.session = [
