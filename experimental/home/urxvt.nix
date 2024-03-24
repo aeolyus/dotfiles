bc22039ee88ecfeb5c5d4765f104495621b81dc0
Richard Huang <richardhuang.huang@gmail.com>
2024-03-24 14:06:04 -0700

    [nix] Format and remove dead code

--- experimental/home/urxvt.nix
+++ experimental/home/urxvt.nix
@@ -4,7 +4,7 @@ let
   # https://github.com/arcticicestudio/nord-xresources
   nord0 = "#2E3440";
   nord1 = "#3B4252";
-  nord2 = "#434C5E";
+  # nord2 = "#434C5E";
   nord3 = "#4C566A";
   nord4 = "#D8DEE9";
   nord5 = "#E5E9F0";
@@ -12,9 +12,9 @@ let
   nord7 = "#8FBCBB";
   nord8 = "#88C0D0";
   nord9 = "#81A1C1";
-  nord10 = "#5E81AC";
+  # nord10 = "#5E81AC";
   nord11 = "#BF616A";
-  nord12 = "#D08770";
+  # nord12 = "#D08770";
   nord13 = "#EBCB8B";
   nord14 = "#A3BE8C";
   nord15 = "#B48EAD";
