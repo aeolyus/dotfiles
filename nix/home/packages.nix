89649b8a0694859319dde68e23fa890d02e5bb4c
Richard Huang <richardhuang.huang@gmail.com>
2024-03-24 23:48:09 -0700

    [nix][home][packages] Add neovim

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    brightnessctl # Control device brightness
     fd # Alternative to find
     file # File type identification utility
+    git-branchless # Enhance git stacked diffs
     gnumake # GNU Make
     htop # Interactive process viewer
+    neovim # Text editor
     pavucontrol # PulseAudio volume control
     pulseaudio # Sound server
     ripgrep # Alternative to grep
-    git-branchless # Enhance git stacked diffs
-    brightnessctl # Control device brightness
   ];
 }
