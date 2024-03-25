a7a93a004a0f986143592dabd2cd42647c3cb9d5
Richard Huang <richardhuang.huang@gmail.com>
2024-03-24 22:03:30 -0700

    [nix][nixos][audio] Disable pipewire x11 bell

--- nix/nixos/audio.nix
+++ nix/nixos/audio.nix
@@ -5,5 +5,13 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+    # No beeping
+    extraConfig.pipewire = {
+      "99-silent-bell" = {
+        "context.properties" = {
+          "module.x11.bell" = false;
+        };
+      };
+    };
   };
 }
