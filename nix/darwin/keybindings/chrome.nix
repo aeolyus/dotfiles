b7afe0647e3096cb984ca1ad339bcfb439bb8fa3
Richard Huang <richardhuang.huang@gmail.com>
2024-03-27 01:37:33 -0700

    [nix][darwin][keybindings] Add chrome keybindings

---
+++ nix/darwin/keybindings/chrome.nix
@@ -0,0 +1,21 @@
+{
+  system.defaults.CustomUserPreferences = {
+    "com.google.Chrome" = {
+      "NSUserKeyEquivalents" = {
+        "New Tab" = "^t";
+        "New Window" = "^n";
+        "Close Tab" = "^w";
+        "Reopen Closed Tab" = "^\$t";
+        "Reload This Page" = "^r";
+        "Cut" = "^x";
+        "Copy" = "^c";
+        "Paste" = "^v";
+        "Select All" = "^a";
+        "Redo" = "^y";
+        "Undo" = "^z";
+        "Find..." = "^f";
+        "Open Location..." = "^l";
+      };
+    };
+  };
+}
