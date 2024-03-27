333b61d1bf861e730b528e4ba81f6a46a3cb49f5
Richard Huang <richardhuang.huang@gmail.com>
2024-03-26 21:00:27 -0700

    [nix][darwin][yabai] Use abs path to yabai binary for config

--- nix/darwin/services/yabai.nix
+++ nix/darwin/services/yabai.nix
@@ -1,3 +1,7 @@
+{ pkgs, ... }:
+let
+  yabai = "${pkgs.yabai}/bin/yabai";
+in
 {
   services.yabai = {
     enable = true;
@@ -28,12 +32,14 @@
     };
     extraConfig = ''
       # Float system settings
-      yabai -m rule --add app="^System Settings$" manage=off
-      yabai -m rule --add app="^System Information$" label="^About This Mac$" manage=off
+      ${yabai} -m rule --add app="^System Settings$" manage=off
+      ${yabai} -m rule --add app="^System Information$" \
+        label="^About This Mac$" manage=off
 
       # Load scripting addition
-      yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
-      sudo yabai --load-sa
+      ${yabai} -m signal --add event=dock_did_restart \
+        action="sudo ${yabai} --load-sa"
+      sudo ${yabai} --load-sa
     '';
   };
 }
