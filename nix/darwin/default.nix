10bfa3334380d6fb06f498a6396bdc5250a9a95b
Richard Huang <richardhuang.huang@gmail.com>
2024-03-27 00:33:07 -0700

    [nix][darwin][keybindings] Swap caps and ctrl

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ../common
+    ./keybindings
     ./nix.nix
     ./services
     ./system.nix
---
+++ nix/darwin/keybindings/default.nix
@@ -0,0 +1,4 @@
+{
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
+}
