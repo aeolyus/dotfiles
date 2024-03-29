e52f743411de8d3ab63fd00415b78a30cde90223
Richard Huang <richardhuang.huang@gmail.com>
2024-03-29 00:01:30 -0700

    [nix][home][nvim] Add nvim config

--- nix/home/base/default.nix
+++ nix/home/base/default.nix
@@ -4,6 +4,7 @@
     ./fzf.nix
     ./git.nix
     ./home.nix
+    ./nvim.nix
     ./packages.nix
     ./tmux.nix
     ./zsh.nix
---
+++ nix/home/base/nvim.nix
@@ -0,0 +1,9 @@
+{
+  # Symlink the current nvim configs
+  # TODO: Look into potentially nixifying nvim or using nixvim
+  xdg.configFile.nvim = {
+    recursive = true;
+    source = ../../../nvim/.config/nvim;
+  };
+}
+
