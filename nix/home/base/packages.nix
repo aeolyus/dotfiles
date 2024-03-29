b6e5d27bc838b9ae64aefe1945ff7da768354ded
Richard Huang <richardhuang.huang@gmail.com>
2024-03-29 11:29:22 -0700

    [nix][home][packages] Add some more core packages

--- nix/home/base/packages.nix
+++ nix/home/base/packages.nix
@@ -1,14 +1,19 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    clang # C language family compiler
     fd # Alternative to find
     file # File type identification utility
     git # Version control system
     git-branchless # Enhance git stacked diffs
     gnumake # GNU Make
+    go # Programming language
     htop # Interactive process viewer
     neovim # Text editor
+    python3 # Programming language
     ripgrep # Alternative to grep
     spotify # Music player
+    unzip # Extraction utility for zip archives
+    zip # Compressor/archiver
   ];
 }
