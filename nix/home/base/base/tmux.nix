62026e0acb56f0888e9e97d5fa53e1c547627a9f
Richard Huang <richardhuang.huang@gmail.com>
2024-04-01 22:39:55 -0700

    [nix][home][tmux] Bind choose-session shortcut

--- nix/home/base/base/tmux.nix
+++ nix/home/base/base/tmux.nix
@@ -29,6 +29,7 @@ in
       bind-key j select-pane -D
       bind-key k select-pane -U
       bind-key l select-pane -R
+      bind-key S choose-session
 
       # Numbering
       setw -g pane-base-index 1
