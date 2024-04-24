d2c87db5eafba1e86ccbe9ba3d53e045e1b55b54
Richard Huang <richardhuang.huang@gmail.com>
2024-04-23 23:10:29 -0700

    [nix][home][i3] Transfer sleep lock for xss-lock

--- nix/home/linux/gui/i3/screen-locker.nix
+++ nix/home/linux/gui/i3/screen-locker.nix
@@ -3,6 +3,9 @@
   services.screen-locker = {
     enable = true;
     xautolock.enable = false;
+    xss-lock.extraOptions = [
+      "--transfer-sleep-lock"
+    ];
     inactiveInterval = 2; # minutes
     lockCmd = "${pkgs.i3lock-fancy-rapid}/bin/i3lock 5 pixel";
   };
