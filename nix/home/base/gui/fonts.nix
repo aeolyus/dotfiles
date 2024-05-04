e81a0f184ee15ff59e391577967f7b7f80eea858
Richard Huang <richardhuang.huang@gmail.com>
2024-05-03 22:53:44 -0700

    [nix][base][gui] Add noto fonts

--- nix/home/base/gui/fonts.nix
+++ nix/home/base/gui/fonts.nix
@@ -2,7 +2,12 @@
 {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
     noto-fonts-emoji
+    noto-fonts-extra
     (nerdfonts.override {
       fonts = [
         "Hack"
