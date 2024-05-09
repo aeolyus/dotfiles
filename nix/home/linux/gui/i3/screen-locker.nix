5cdec76269920436d544468a71a38fddd4742f8c
Richard Huang <richardhuang.huang@gmail.com>
2024-05-08 22:42:53 -0700

    [nix][home][linux][screen-lock] Add xautolock service to handle sleep
    Without using a desktop environment or session manager, logind believes
    that the session is not graphical and it is non-trivial to set the
    session type ourself. See link below for more info.
    https://unix.stackexchange.com/q/209274
    
    Instead of relying on logind for sleep-on-idle, we can utilize xautolock
    to do so instead. The code is copied from screen-locker.nix as there is
    no way to configure both xss-lock and xautolock with home-manager
    currently as hm considers them mutually exclusive.

--- nix/home/linux/gui/i3/screen-locker.nix
+++ nix/home/linux/gui/i3/screen-locker.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
+let
+  cfg = config.services.screen-locker;
+in
 {
   services.screen-locker = {
     enable = true;
@@ -8,5 +11,25 @@
     ];
     inactiveInterval = 5; # minutes
     lockCmd = "${pkgs.i3lock-fancy-rapid}/bin/i3lock 5 pixel --nofork";
+  };
+
+  # Use xautolock to sleep to avoid relying on logind
+  systemd.user.services.xautolock-session = {
+    Unit = {
+      Description = "xautolock, session locker service";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Install = { WantedBy = [ "graphical-session.target" ]; };
+
+    Service = {
+      ExecStart = lib.concatStringsSep " " ([
+        "${cfg.xautolock.package}/bin/xautolock"
+        "-time 30" # minutes
+        "-locker '${pkgs.systemd}/bin/systemctl suspend-then-hibernate'"
+      ] ++ lib.optional cfg.xautolock.detectSleep "-detectsleep"
+      ++ cfg.xautolock.extraOptions);
+    };
   };
 }
