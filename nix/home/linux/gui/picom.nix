7a8093dbe800b3993864e3660f7120526f322561
Richard Huang <richardhuang.huang@gmail.com>
2025-01-06 01:15:06 -0800

    [linux][picom] Fix maim taking dim selection screenshots

--- nix/home/linux/gui/picom.nix
+++ nix/home/linux/gui/picom.nix
@@ -6,6 +6,10 @@
     shadowExclude = [
       "class_g = 'Polybar'"
       "class_g = 'i3bar'"
+      # Prevent maim's selector from shadow and dimming screenshots
+      # https://github.com/naelstrof/maim/issues/40#issuecomment-126933853
+      "name = 'Screenshot'"
+      "class_g = 'slop'"
     ];
   };
 }
