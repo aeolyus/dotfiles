c2b6c57ab2b138409df964191277f2fdaa54b54f
Richard Huang <richardhuang.huang@gmail.com>
2025-02-02 15:05:57 -0800

    [outputs] Add ghost host

---
+++ nix/outputs/hosts/ghost/default.nix
@@ -0,0 +1,25 @@
+{ inputs, vars, ... }:
+let
+  self = inputs.self;
+  home-manager = self.inputs.home-manager;
+  nixosSystem = inputs.nixpkgs.lib.nixosSystem;
+  user = vars.users.aeolyus;
+in
+nixosSystem {
+  system = "x86_64-linux";
+  specialArgs = { inherit self inputs user; };
+  modules = [
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.users.${user.username} = import ../../../home/linux/gui;
+      home-manager.extraSpecialArgs = { inherit user; };
+    }
+    {
+      networking.hostName = "ghost";
+    }
+    ../../../common
+    ../../../nixos
+    ../../../overlays
+    ./hardware-configuration.nix
+  ];
+}
