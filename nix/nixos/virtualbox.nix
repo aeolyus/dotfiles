2a35517e979e3d3c8cafb1a64aa5a44ac865dad4
Richard Huang <richardhuang.huang@gmail.com>
2026-01-26 19:52:14 -0800

    [nixos] Remove virtualbox guest additions

--- nix/nixos/virtualbox.nix
+++ nix/nixos/virtualbox.nix
@@ -2,8 +2,6 @@
 {
   virtualisation.virtualbox = {
     host.enable = true;
-    # Guest Extensions optimize the guest OS for better perf and usability
-    guest.enable = true;
   };
   users.extraGroups.vboxusers.members = [ user.username ];
 }
